efc8ac858131230b03e351e89d1a4301da2a6e39
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-11-11 16:55:27 +0900

    1주차 과제 완성

---
+++ src/firstHomework_Receipt.java
@@ -0,0 +1,18 @@
+public class firstHomework_Receipt {
+    public static void main(String[] args) {
+        int point = 4;
+        double percent = ((double) point/5) * 100;
+        System.out.println("[대충 맛있는 알리올리오 만들기]");
+        System.out.println("별점 : " + point + "(" + percent + "%)");
+        System.out.println(1 + ". 마늘 취향껏, 액젓, 치킨스톡, 설탕, 페퍼론치노 준비해주세요");
+        System.out.println(2 + ". 팬에 올리브유를 적당히 둘러주세요");
+        System.out.println(3 + ". 통마늘이 있을 경우, 통마늘부터 약불로 조리를 시작합니다.");
+        System.out.println(4 + ". 편마늘을 넣고, 4분정도 약불로 조리하며 마늘의 향을 기름에 입힙니다.");
+        System.out.println(5 + ". 다진마늘을 넣고, 마늘이 조금 투명한 색을 띌 때까지 조리합니다.");
+        System.out.println(6 + ". 물 120ml를 넣어주세요");
+        System.out.println(7 + ". 직후 치킨스톡 1Ts, 설탕 1Ts, 액젓 0.5Ts, 페퍼론치노 취향껏 넣고, 중불로 올립니다");
+        System.out.println(8 + ". 1분 이내로 조리하시다가, 끓는 물에 7분정도 삶은 파스타면을 넣어주세요. 직후 강불로 올립니다.");
+        System.out.println(9 + ". 면의 전분과 물, 기름이 만나 소스같은 느낌이 될 때까지 저어주시면, 완성입니다.");
+        System.out.println(10 + ". 기호에 따라 후추를 뿌려 드셔도 맛있습니다.");
+    }
+}
