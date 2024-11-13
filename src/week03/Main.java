3a146b20e9111b13008d56881a65c5209e680b7a
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-11-13 14:24:36 +0900

    3-4 강의 완료한 상태

--- src/week03/Main.java
+++ src/week03/Main.java
@@ -3,28 +3,21 @@ package week03;
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
-        
-        //초기값 기본 값 확인
-        System.out.println("car.model = " + car.model);
-        System.out.println("car.color = " + car.color);
-        System.out.println(        );
 
-        System.out.println("car.speed = " + car.speed);
+        System.out.println("페달 밟기 전 car.gear = " + car.gear);
+
+        double speed = car.gasPedal(100, 'D');
+        System.out.println("speed = " + speed);
+
+        boolean lights = car.onOffLight();
+        System.out.println("lights = " + lights);
+
+        System.out.println();
         System.out.println("car.gear = " + car.gear);
-        System.out.println("car.lights = " + car.lights);
+
         System.out.println();
-
-        System.out.println("car.tire = " + car.tire);
-        System.out.println("car.door = " + car.door);
+        car.carSpeed(100, 80);
         System.out.println();
-
-        car.color = "blue";
-        car.speed = 100;
-        car.lights = false;
-
-        System.out.println("car.color = " + car.color);
-        System.out.println("car.speed = " + car.speed);
-        System.out.println("car.lights = " + car.lights);
-
+        car.carSpeed(100, 120, 150);
     }
 }
