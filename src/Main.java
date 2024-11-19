2a7bb55ff197d12ce8c2937f380979237099c42e
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-11-19 16:31:43 +0900

    1241119 세션 들으면서 같이 끄적

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    static class Person {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // 1. 속성
+        String name;
+        int age;
+        String address;
+
+        // 2. 생성자
+        Person(String name, int age) {
+            this.name = name;
+            this.age = age;
         }
+
+        // 3-1. 소개 기능(이름 나이 출력 기능)
+        void introduce() {
+            System.out.println("나의 이름은");
+            System.out.println(this.name + "입니다.");
+            System.out.println("나의 나이는");
+            System.out.println(this.age + "입니다.");
+        }
+
+        // 3-2. 더하기 기능(소개를 하고 더하기 연산 수행)
+        int sum(int value1, int value2) {
+            introduce();
+            int result = value1 + value2;
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        String name = "Steve";
+        int age = 20;
+        Person personA = new Person(name, age);
+        personA.introduce();
+
+        int value1 = 1;
+        int value2 = 2;
+
+        int ret = personA.sum(value1, value2);
+        System.out.println(ret);
     }
 }
